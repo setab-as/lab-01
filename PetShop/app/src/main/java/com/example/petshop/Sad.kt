b10387d4512d145b896341c133f9f8814b7bbe20
Avnil Sharma <avnilsharma26@gmail.com>
2026-09-03 14:32:18 -0600

    Simplify sad feeling message

--- PetShop/app/src/main/java/com/example/petshop/Sad.kt
+++ PetShop/app/src/main/java/com/example/petshop/Sad.kt
@@ -2,6 +2,6 @@ package com.example.petshop
 
 class Sad(date: String): Mood(date) {
     override fun feeling(): String {
-        return "I feel sad. :( $date"
+        return "I feel sad. :("
     }
 }
